26832c3a4f94a24f28691518c2aa2d60ff74b7c0
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-03 19:01:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>Getting started with Redux Toolkit</>;
+}
+
+export default App;
